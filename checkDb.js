8bff291800f1e3fc1e6e0b5db88c80e0e32324c1
rithik <115656594+theRithik@users.noreply.github.com>
2023-04-17 21:06:51 +0530

    Update checkDb.js

--- checkDb.js
+++ checkDb.js
@@ -4,3 +4,4 @@ async function showDataBase(client){
     const dataList = await client.db().admin().listDatabases();
     console.log('dbs', dataList)
      dataList.databases.forEach(db=> console.log(` -${db.name}`))
+}
