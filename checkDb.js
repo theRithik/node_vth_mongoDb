158179e1073318d1912861a8cfff77a13193293d
rithik <115656594+theRithik@users.noreply.github.com>
2023-04-17 21:06:37 +0530

    Create checkDb.js

---
+++ checkDb.js
@@ -0,0 +1,6 @@
+const { MongoClient} = require('mongodb')
+
+async function showDataBase(client){
+    const dataList = await client.db().admin().listDatabases();
+    console.log('dbs', dataList)
+     dataList.databases.forEach(db=> console.log(` -${db.name}`))
